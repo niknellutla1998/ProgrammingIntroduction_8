95861e198883d37fce52bbbd1ec20f6a8a905ded
niknellutla1998 <nikbat3009@gmail.com>
2024-09-10 21:33:55 -0400

    Hi nik

--- ProgrammingIntroduction_8/Program.cs
+++ ProgrammingIntroduction_8/Program.cs
@@ -1,7 +1,7 @@
 ﻿// All .NET programs begin with the main method, but with .NET 6 they removed the need for Main function
 
 using System.Diagnostics;
-
+// change made in class by nik 
 
 // example 0 - first program
 Console.WriteLine("Hello World!");
